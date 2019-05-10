7ca9185f787d593a5931af23e16323a02db838b3
pubuzhixing8 <pubuzhixing@gmail.com>
2019-05-10 14:57:19 +0800

    fix(modal): fix modal bug shouldn't close modal when click modal and leave

--- src/modal/modal.js
+++ src/modal/modal.js
@@ -90,6 +90,8 @@ angular.module('ui.bootstrap.modal', ['ui.bootstrap.multiMap', 'ui.bootstrap.sta
         scope.size = attrs.size;
 
         scope.close = function(evt) {
+          console.log(evt.target);
+          console.log(evt.currentTarget);
           var modal = $modalStack.getTop();
           if (modal && modal.value.backdrop &&
             modal.value.backdrop !== 'static' &&
@@ -101,7 +103,7 @@ angular.module('ui.bootstrap.modal', ['ui.bootstrap.multiMap', 'ui.bootstrap.sta
         };
 
         // moved from template to fix issue #2280
-        element.on('click', scope.close);
+        element.on('mousedown', scope.close);
 
         // This property is only added to the scope for the purpose of detecting when this directive is rendered.
         // We can detect that by using this property in the template associated with this directive and then use
